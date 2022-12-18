92e063a560204f83c7614353b48141c80fa40095
chimezdev <ochuba6@gmail.com>
2022-12-18 12:35:22 +0100

    set debug to false

--- my_debtors_core/settings.py
+++ my_debtors_core/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
